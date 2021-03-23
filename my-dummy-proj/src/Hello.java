ba5ec4b623652e74432a7fae8b86ea85c277a423
Chetan <chetansp0909@gmail.com>
2021-03-23 18:32:49 +0530

    sop2 is editted

--- my-dummy-proj/src/Hello.java
+++ my-dummy-proj/src/Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
 	public void method1() {
 		System.out.println("Hello World");
-		System.out.println("Hello World2");
+		System.out.println("Hello World2 is edited");
 		System.out.println("in sop from different wp");
 		System.out.println("in sop from different wp1");
 	}
